2365880e14ac357ad6f9209a104ebde3b0c5feb0
jagriti11 <jagritikhantwal@gmail.com>
2024-03-04 22:06:28 +0530

    some changes in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateinvoiceComponent } from './createinvoice/createinvoice.component';
 import { CustomerComponent } from './customer/customer.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateinvoiceComponent,
-    CustomerComponent
+    CustomerComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
