fc824fc2a605af5a1f919a517c462d61736e63a0
Davinder Pal <davinder@solidevelectrosoft.com>
2024-05-17 16:10:06 +0530

    chore: Update styles.css, footer.component.css, spinner.component.html, spinner.component.css, app.component.html, spinner.component.ts, auth.service.ts, loader.service.ts, spinner.component.spec.ts, loader.interceptor.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'https://localhost:7004/api/Authentication'; // Replace with your API URL
+  private apiUrl = `${environment.apiUrl}Authentication`;
 
   constructor(private http: HttpClient, private router: Router) { }
 
